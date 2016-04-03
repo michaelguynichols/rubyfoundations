493fe4ac0fc37b959f5552bb07d49fa36c1b1919
michaelguynichols <michael.guy.nichols@gmail.com>
2016-04-03 02:24:21 +0200

    Prime updated

--- lesson2/nth_prime.rb
+++ lesson2/nth_prime.rb
@@ -1,28 +1,24 @@
 class Prime
-  RANGE = 1000000000
   START = 2
-
-  def initialize()
-    @n_index = nil
-    @primes = nil
-  end
 
   def self.nth(up_to_n)
     raise ArgumentError if up_to_n <= 0
     @n_index = up_to_n - 1
     @primes = []
+    @range = START ** up_to_n
     generate_primes
     self.nth_prime
   end
+
+  private
 
   def self.nth_prime
     @primes[@n_index]
   end
 
-  private
-
   def self.generate_primes
-    (START..RANGE).each do |possible_prime|
+    end_of_range = @range
+    (START..end_of_range).each do |possible_prime|
       break if @primes.size >= @n_index + 1
       @primes << possible_prime if self.prime_number?(possible_prime)
     end
